e09c8fb7d61fddcf3794ad3269125d0fa6ed0327
kylefb1050 <kyle.bandong01@gmail.com>
2023-09-04 22:09:27 -0400

    Homework2

--- Week2HW/homework2.js
+++ Week2HW/homework2.js
@@ -7,21 +7,15 @@ let numberOfToppings = 10;
 console.log(numberOfToppings);
 
 console.log(
-  "Hello, welcome to " +
-    pizzaPlace +
-    ", where we offer a whopping " +
-    numberOfToppings +
-    " toppings per pie!"
+  `Welcome to ${pizzaPlace}, where we offer ${numberOfToppings} toppings a pie.`
 );
 
 console.log("How many toppings would you like on your pizza?");
 
 let numberOfToppings1 = 10;
 
-if (!numberOfToppings1 > 11) {
+if (!numberOfToppings1 <= 10) {
   console.log("Quality, not quantity.");
 } else {
   console.log("A whole lot of pizza");
 }
-
-while ()
