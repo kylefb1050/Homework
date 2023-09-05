bd0801eaa596c671ab09996ee5b7d28ddcc20755
kylefb1050 <kyle.bandong01@gmail.com>
2023-09-05 18:09:05 -0400

    homework2fix

--- Week2HW/homework2.js
+++ Week2HW/homework2.js
@@ -1,13 +1,19 @@
 //Kyle Bandong Homework 2
 
+const { log } = require("console");
+
 let pizzaPlace = "Pies of Sicily";
+
 console.log(pizzaPlace);
+console.log(typeof pizzaPlace);
 
 let numberOfToppings = 10;
+
 console.log(numberOfToppings);
+console.log(typeof numberOfToppings);
 
 console.log(
-  `Welcome to ${pizzaPlace}, where we offer ${numberOfToppings} toppings a pie.`
+  `Welcome to ${pizzaPlace}, where we offer ${numberOfToppings} toppings per pizza.`
 );
 
 console.log("How many toppings would you like on your pizza?");
